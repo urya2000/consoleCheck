1498a168730622ad016fa525eaf352684c1c74fe
urya2000 <suryaprakashmessi99@gmail.com>
2025-01-20 11:02:50 +0530

    first file

---
+++ hello_world/src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+
+import Footer from "./components/Footer";
+import Main from "./components/Main";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
