b67f5c449ced92c6a601cd84663838451db7cedd
urya2000 <suryaprakashmessi99@gmail.com>
2025-01-20 11:14:39 +0530

    update folder

--- hello_world/src/App.js
+++ hello_world/src/App.js
@@ -4,12 +4,14 @@ import Header from "./components/Header";
 
 import Footer from "./components/Footer";
 import Main from "./components/Main";
+import About from "./components/About";
 
 function App() {
   return (
     <>
       <Header />
       <Main />
+      <About />
       <Footer />
     </>
   );
